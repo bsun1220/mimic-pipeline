11b63424887e908a0900a913a21642860c4c871e
MuhangTian <muhang.tian@duke.edu>
2023-02-14 18:22:19 -0500

    few changes

--- src/disease/Disease.py
+++ src/disease/Disease.py
@@ -1,4 +1,9 @@
+'''
+Author: Tony
+Classes for each disease in order to find patients and relevant features
+'''
 from abc import ABC, abstractmethod
+from loader import Loader
 
 class Disease(ABC):
     
@@ -6,10 +11,38 @@ class Disease(ABC):
         ...
         pass
     
-    
-    
     @abstractmethod
-    def predict_proba():
+    def get_patients():
+        """
+        Obtain all patients with this particular disease
+        """
         pass
+
+
+class Sepsis(Disease):
+    
+    def __init__(self):
+        super().__init__()
+        pass
+    
+
+class HeartAttack(Disease):
+    
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+class HeartFailure(Disease):
+    
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    
+if __name__=="__main__":
+    import sys
+    print(sys.path)
+    loader = Loader()
     
     
